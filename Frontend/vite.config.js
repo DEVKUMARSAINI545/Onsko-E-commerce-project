de1e4c3844ed991199be3e2c477f30bdc5ca1aa1
dev kumar saini <devsaini27806@gmail.com>
2025-03-30 17:26:01 +0530

    added new backend URL

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
    server:{
      proxy:{
-      "/api/v1/onsko":"https://e-commerce-project-dw75.onrender.com"
+      "/api/v1/onsko":"https://onsko-e-commerce-project.onrender.com"
       // "/api/v1/onsko":"http://localhost:3000"
      }
    },
