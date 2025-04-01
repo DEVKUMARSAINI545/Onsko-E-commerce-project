511e89435b262711abc8a715b4532a5cf4758907
dev kumar saini <devsaini27806@gmail.com>
2025-04-01 18:51:28 +0530

    app.js updated added frontend vercal URL

--- Backend/app.js
+++ Backend/app.js
@@ -22,7 +22,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieparser());
 const corsOptions = {
-  origin: 'http://localhost:5173', // Allow your frontend's URL
+  origin: 'onsko-e-commerce-project.vercel.app', // Allow your frontend's URL
   
   credentials: true, // Allow cookies to be sent with requests
    
