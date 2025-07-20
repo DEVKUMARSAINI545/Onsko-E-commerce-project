99b46bb7f2092de0ffd4910433db959fc516777e
Dev Saini <137302375+DEVKUMARSAINI545@users.noreply.github.com>
2025-07-20 11:35:18 +0530

    Update axios.js

--- Frontend/src/axios.js
+++ Frontend/src/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Create an axios instance with default configurations
 const axiosInstance = axios.create({
   // baseURL: 'https://onsko-e-commerce-project.onrender.com/api/v1/onsko', // The base URL of your backend
-  baseURL: import.meta.env.VITE_API_URL, // The base URL of your backend
+  baseURL: 'http://13.203.104.149:3000/api/v1/onsko', // The base URL of your backend
   withCredentials: true,  // Automatically send credentials (cookies) with every request
   headers: {
     'Content-Type': 'application/json',
