9b95ef4ec84f9665d960c66ef900f74b57739594
DEVKUMARSAINI <devsaini27806@gmail.com>
2025-07-30 12:07:18 +0000

    latest changes by ec2

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-   server:{
-     proxy:{
-      // "/api/v1/onsko":"https://onsko-e-commerce-project.onrender.com"
-      "/api/v1/onsko":"http://localhost:3000"
-     }
-   },
-  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    proxy: {
+      "/api/v1/onsko": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
+  plugins: [react()]
 })
+
